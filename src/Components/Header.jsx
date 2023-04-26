54d30dfa622b39c6f71c78ad11153d6c94ca39f0
Shahriar Hasan Chowdhury <shahriarmoin.aust@gmail.com>
2023-04-26 16:01:59 +0600

    login register page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
-                <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+                <Link className="btn btn-ghost normal-case text-xl">Auth Master</Link>
                 <Link to="/" className="btn btn-ghost normal-case text-xl">Home</Link>
                 <Link to="/Login" className="btn btn-ghost normal-case text-xl">Login</Link>
                 <Link to="/Register" className="btn btn-ghost normal-case text-xl">Register</Link>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handleregister = (event) => {
+
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const name = form.name.value;
+        const password = form.password.value;
+
+        console.log(name, email, password);
+    }
+
+
+
     return (
-        <div>
-            please register
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Please Register now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body" onSubmit={handleregister}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" required placeholder="Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" required placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" required placeholder="password" className="input input-bordered" />
+
+                            <label className="label">
+                                <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                            </label>
+                            <label className="label">
+                                <Link to="/login" className="label-text-alt link link-hover">Already have an account?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
